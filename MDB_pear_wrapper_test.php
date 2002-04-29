68b44e3eb1a08c61f40270005b14c8f6c5702658
Lukas Smith <lsmith@php.net>
2002-04-29 22:12:05 +0000

    initial version
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/trunk@80501 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ MDB_pear_wrapper_test.php
@@ -0,0 +1,110 @@
+<?php
+ /*
+ *
+ * @(#) $Header$
+ *
+ */
+    require_once("pear_wrapper.php");
+    require_once("Var_Dump.php");
+
+    // just for kicks you can mess up this part to see some pear error handling
+    $user = 'metapear';
+    $pass = 'funky';
+    //$pass = "";
+    $host = 'localhost';
+    $db_name = 'metapear_test_db';
+    // Data Source Name: This is the universal connection string
+    $dsn = "mysql://$user:$pass@$host/$db_name";
+    // MDB::connect will return a Pear DB object on success
+    // or a Pear DB Error object on error
+    // You can also set to TRUE the second param
+    // if you want a persistent connection:
+    // $db = DB::connect($dsn, true);
+    $db = DB::connect($dsn);
+    // With DB::isError you can differentiate between an error or
+    // a valid connection.
+    //echo Var_Dump::display($db)."<br>";
+    if (DB::isError($db)) {
+        die ($db->getMessage());
+    }
+
+    // happy query
+    $query ="SELECT * FROM test";
+    echo "query for the following examples:".$query."<br>";
+    echo "<br>field:<br>".$db->getOne($query)."<br>";
+
+    // run the query and get a result handler
+    $result = $db->query($query);
+    echo "<br>tableInfo() ";
+    Var_Dump::display($result->tableInfo());
+
+    $result = $db->query($query);
+    echo "<br>numCols() ";
+    Var_Dump::display($result->numCols());
+    $result->fetchInto(&$arr);
+    echo "<br>fetchInto(&$arr) ";
+    Var_Dump::display($arr);
+    echo "<br>free() ";
+    Var_Dump::display($result->free());
+
+    $result = $db->query($query);
+    echo "<br>nextResult() ";
+    Var_Dump::display($result->nextResult());
+	if($result->nextResult())
+	{
+		echo "true";
+	} else {
+		echo "false";
+	}
+    echo "<br>numRows() ";
+    Var_Dump::display($result->numRows());
+    echo "<br>fetchRow() ";
+    Var_Dump::display($result->fetchRow());
+
+    // lets create a sequence on demand
+	echo "<br>get the next id using on demand:<br>";
+    echo "<br>nextId:".$db->nextId("real_funky_id_2");
+	echo "<br>dropSequence:".$db->dropSequence("real_funky_id_2");
+
+    // lets create a sequence
+    echo "<br>create a new seq with start 3 name real_funky_id<br>";
+    $err = $db->createSequence("real_funky_id",3);
+    if (DB::isError($err)) {
+        echo "<br>could not create sequence again<br>";
+    }
+
+    echo "<br>get the next id:<br>";
+    echo $db->nextId("real_funky_id");
+    echo "<br>affected rows:<br>";
+    echo $db->affectedRows()."<br>";
+    // lets try an prepare execute combo
+    $alldata = array(  array(1, 'one', 'un'),
+                       array(2, 'two', 'deus'),
+                       array(3, 'three', 'trois'),
+                       array(4, 'four', 'quatre'));
+    $prepared_query = $db->prepare("INSERT INTO numbers VALUES(?,?,?)");
+    foreach ($alldata as $row) {
+        echo "running execute<br>";
+        $db->execute($prepared_query, $row);
+    }
+    // lets try an prepare execute combo
+    $alldata = array(  array(5, 'five', 'cinq'),
+                       array(6, 'six', 'six'),
+                       array(7, 'seven', 'sept'),
+                       array(8, 'eight', 'huit'));
+    $prepared_query = $db->prepare("INSERT INTO numbers VALUES(?,?,?)");
+    $db->executeMultiple($prepared_query, $alldata);
+    echo "running executeMultiple<br>";
+    $array = array(4);
+    echo "<br>see getOne in action:<br>".$db->getOne("SELECT trans_en FROM numbers WHERE number = ?",$array)."<br>";
+    // You can disconnect from the database with:
+    echo "<br>see getRow in action:<br>";
+    echo Var_Dump::display($db->getRow("SELECT * FROM numbers WHERE number = ?",$array))."<br>";
+    echo "<br>see getCol in action:<br>";
+    echo Var_Dump::display($db->getCol("SELECT * FROM numbers",$array))."<br>";
+    echo "<br>see getAssoc in action:<br>";
+    echo Var_Dump::display($db->getAssoc("SELECT * FROM test", false, "", DB_FETCHMODE_ASSOC))."<br>";
+    echo "tableInfo on a string:<br>";
+    echo Var_Dump::display($db->tableInfo("numbers"))."<br>";
+    $db->disconnect();
+?>
---
+++ pear_wrapper.php
@@ -0,0 +1,376 @@
+<?php
+ /*
+ *
+ * @(#) $Header$
+ *
+ */
+ 
+require_once "MDB.php";
+
+class DB
+{
+    function &factory($type)
+    {
+        $MDB_object = MDB::factory($type);
+        if(PEAR::isError($MDB_object))
+        {
+            return $MDB_object;
+        }
+        $obj =& new MDB_PEAR_PROXY($MDB_object);
+        return $obj;
+    }
+
+    function &connect($dsn, $options = false)
+    {
+        $MDB_object = MDB::connect($dsn, $options);
+        if(PEAR::isError($MDB_object))
+        {
+            return $MDB_object;
+        }
+        $obj =& new MDB_PEAR_PROXY($MDB_object);
+        return $obj;
+    }
+
+    function apiVersion()
+    {
+        return MDB::apiVersion();
+    }
+
+    function isError($value)
+    {
+        return MDB::isError($value);
+    }
+
+    function isManip($query)
+    {
+        return MDB::isManip($query);
+    }
+
+    function isWarning($value)
+    {
+        return MDB::isWarning($value);
+    }
+
+    function errorMessage($value)
+    {
+        return MDB::errorMessage($value);
+    }
+
+    function parseDSN($dsn)
+    {
+        return MDB::parseDSN($dsn);
+    }
+
+    function assertExtension($name)
+    {
+        return MDB::assertExtension($name);
+    }
+}
+
+class DB_Error extends PEAR_Error
+{
+    function DB_Error($code = DB_ERROR, $mode = PEAR_ERROR_RETURN,
+              $level = E_USER_NOTICE, $debuginfo = null)
+    {
+        if (is_int($code)) {
+            $this->PEAR_Error('DB Error: ' . DB::errorMessage($code), $code, $mode, $level, $debuginfo);
+        } else {
+            $this->PEAR_Error("DB Error: $code", DB_ERROR, $mode, $level, $debuginfo);
+        }
+    }
+}
+
+class DB_Warning extends PEAR_Error
+{
+    function DB_Warning($code = DB_WARNING, $mode = PEAR_ERROR_RETURN,
+            $level = E_USER_NOTICE, $debuginfo = null)
+    {
+        if (is_int($code)) {
+            $this->PEAR_Error('DB Warning: ' . DB::errorMessage($code), $code, $mode, $level, $debuginfo);
+        } else {
+            $this->PEAR_Error("DB Warning: $code", 0, $mode, $level, $debuginfo);
+        }
+    }
+}
+
+class DB_result
+{
+    var $dbh;
+    var $result;
+    var $row_counter = null;
+
+    var $limit_from  = null;
+
+    var $limit_count = null;
+
+    function DB_result(&$dbh, $result)
+    {
+        $this->dbh = &$dbh;
+        $this->result = $result;
+    }
+
+    function fetchRow($fetchmode = DB_FETCHMODE_DEFAULT, $rownum= 0)
+    {
+		$this->dbh->fetchInto($this->result, &$arr, $fetchmode, $rownum);
+		if(is_array($arr)) {
+			return $arr;
+		}
+		else {
+			return null;
+		}
+    }
+
+    function fetchInto(&$arr, $fetchmode = DB_FETCHMODE_DEFAULT, $rownum = 0)
+    {
+		$this->dbh->fetchInto($this->result, &$arr, $fetchmode, $rownum);
+    }
+
+    function numCols()
+    {
+        return $this->dbh->numCols($this->result);
+    }
+
+    function numRows()
+    {
+        return $this->dbh->numRows($this->result);
+    }
+
+    function nextResult()
+    {
+        return $this->dbh->nextResult($this->result);
+    }
+
+    function free()
+    {
+        $err = $this->dbh->freeResult($this->result);
+        if(DB::isError($err)) {
+            return $err;
+        }
+        $this->result = false;
+        return true;
+    }
+
+    function tableInfo($mode = null)
+    {
+        return $this->dbh->tableInfo($this->result, $mode);
+    }
+
+    function getRowCounter()
+    {
+        return $this->row_counter;
+    }
+}
+
+class DB_row
+{
+    function DB_row(&$arr)
+    {
+        for (reset($arr); $key = key($arr); next($arr)) {
+            $this->$key = &$arr[$key];
+        }
+    }
+}
+
+class MDB_PEAR_PROXY
+{
+	var $MDB_object;
+	
+	function MDB_PEAR_PROXY($MDB_object)
+	{
+		$this->MDB_object = $MDB_object;
+	}
+
+    function connect($dsninfo, $persistent = false)
+    {
+		return $this->MDB_object->connect($dsninfo, $persistent);
+    }
+
+    function disconnect()
+    {
+		return $this->MDB_object->disconnect();
+    }
+
+	function quoteString($string)
+	{
+		return $this->MDB_object->quoteString($string);
+	}
+
+	function quote($string)
+	{
+		return $this->MDB_object->quote($string);
+	}
+
+	function provides($feature)
+	{
+		return $this->MDB_object->provides($feature);
+	}
+
+	function errorCode($nativecode)
+	{
+		return $this->MDB_object->errorCode($nativecode);
+	}
+
+	function errorMessage($dbcode)
+	{
+		return $this->MDB_object->errorMessage($dbcode);
+	}
+
+	function &raiseError($code = DB_ERROR, $mode = null, $options = null,
+						 $userinfo = null, $nativecode = null)
+	{
+		return $this->MDB_object->raiseError($code = DB_ERROR, $mode, $options, $userinfo, $nativecode);
+	}
+
+	function setFetchMode($fetchmode, $object_class = null)
+	{
+		return $this->MDB_object->setFetchMode($fetchmode, $object_class);
+	}
+
+	function setOption($option, $value)
+	{
+		return $this->MDB_object->setOption($option, $value);
+	}
+
+	function getOption($option)
+	{
+		return $this->MDB_object->getOption($option);
+	}
+
+	function prepare($query)
+	{
+		return $this->MDB_object->prepare($query);
+	}
+
+	function execute($stmt, $data = false)
+	{
+		$result = $this->MDB_object->execute($stmt, $data);
+		if (DB::isError($result) || $result === DB_OK) {
+			return $result;
+		} else {
+			return new DB_result($this->MDB_object, $result);
+		}
+	}
+
+	function executeMultiple( $stmt, &$data )
+	{
+		return $this->MDB_object->executeMultiple( $stmt, &$data );
+	}
+
+	function &query($query, $params = array()) {
+		$result = $this->MDB_object->query($query, $params);
+		if (DB::isError($result) || $result === DB_OK) {
+			return $result;
+		} else {
+			return new DB_result($this->MDB_object, $result);
+		}
+	}
+
+	function limitQuery($query, $from, $count)
+	{
+		$result = $this->MDB_object->limitQuery($query, $from, $count);
+		if (DB::isError($result) || $result === DB_OK) {
+			return $result;
+		} else {
+			return new DB_result($this->MDB_object, $result);
+		}
+	}
+
+	function &getOne($query, $params = array())
+	{
+		return $this->MDB_object->getOne($query, $params);
+	}
+
+	function &getRow($query,
+					 $params = null,
+					 $fetchmode = DB_FETCHMODE_DEFAULT)
+	{
+		return $this->MDB_object->getRow($query, $params, $fetchmode);
+	}
+
+
+	function &getCol($query, $col = 0, $params = array())
+	{
+		return $this->MDB_object->getCol($query, $col, $params);
+	}
+
+
+	function &getAssoc($query, $force_array = false, $params = array(),
+					   $fetchmode = DB_FETCHMODE_ORDERED, $group = false)
+	{
+		return $this->MDB_object->getAssoc($query, $force_array, $params, $fetchmode, $group);
+	}
+
+	function &getAll($query,
+					 $params = null,
+					 $fetchmode = DB_FETCHMODE_DEFAULT)
+	{
+		return $this->MDB_object->getAll($query, $params, $fetchmode);
+	}
+
+	function autoCommit($onoff=false)
+	{
+		return $this->MDB_object->autoCommit($onoff);
+	}
+
+	function commit()
+	{
+		return $this->MDB_object->commit();
+	}
+
+	function rollback()
+	{
+		return $this->MDB_object->rollback();
+	}
+
+	function numRows($result)
+	{
+		return $this->MDB_object->numRows($result);
+	}
+
+	function affectedRows()
+	{
+		return $this->MDB_object->affectedRows();
+	}
+
+	function errorNative()
+	{
+		return $this->MDB_object->errorNative();
+	}
+
+	function nextId($seq_name, $ondemand = true)
+	{
+		$return = $this->MDB_object->nextId($seq_name);
+		if($ondemand && $return == 0)
+		{
+			$this->MDB_object->createSequence($seq_name, 1);
+			$return = $this->MDB_object->nextId($seq_name);
+		}
+		return $return;
+	}
+
+	function createSequence($seq_name)
+	{
+		return $this->MDB_object->createSequence($seq_name, 1);
+	}
+
+	function dropSequence($seq_name)
+	{
+		return $this->MDB_object->dropSequence($seq_name);
+	}
+
+	function tableInfo($result, $mode = null)
+	{
+		return $this->MDB_object->tableInfo($result, $mode);
+	}
+
+	function getTables()
+	{
+		return $this->MDB_object->getTables();
+	}
+
+	function getListOf($type)
+	{
+		return $this->MDB_object->getListOf($type);
+	}
+}
+?>
