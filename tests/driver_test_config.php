2cf6ee478113e227c9aec13b16a439ebf0e13094
Lukas Smith <lsmith@php.net>
2002-06-28 14:15:48 +0000

    added license information added config settings for pgsql
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/trunk@87002 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/driver_test_config.php
+++ tests/driver_test_config.php
@@ -1,11 +1,27 @@
 
 <?
-/*
- * driver_test_configuration.php
- *
- * @(#) $Header$
- *
- */
+//
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2002 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Manuel Lemos <mlemos@acm.org>                               |
+// |                                                                      |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
+// Metabase test suite config script.
+//
 
 	$driver_arguments["Type"]="mysql";
 	$driver_arguments["DontCaptureDebug"]=1;
@@ -77,7 +93,9 @@
 			$database_variables["name"]="userdsn";
 			break;
 		case "pgsql":
-			$driver_arguments["User"]="root";
+			$driver_arguments["User"]="metapear";
+			$driver_arguments["Password"]="funky";
+			$driver_arguments["Options"]["UseTransactions"]=1;
 			break;
 	}
 ?>
