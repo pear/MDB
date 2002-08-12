05fc48c329db3a8d8f743153f8cd6aaab28a6890
Lukas Smith <lsmith@php.net>
2002-08-12 12:28:15 +0000

    PEAR CS fixes
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/trunk@91616 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/setup_test.php
+++ tests/setup_test.php
@@ -45,6 +45,9 @@
 // $Id$
 //
 
+require("manager.php");
+require("metabase_wrapper.php");
+
 /**
 * Part of Metabase test suite
 *
@@ -52,44 +55,42 @@
 * @author  Manuel Lemos <mlemos@acm.org>>
 */
 
-    require("manager.php");
-    require("metabase_wrapper.php");
-
-Function Output($message)
+function Output($message)
 {
     echo $message,"\n";
 }
 
-Function Dump($output)
+function Dump($output)
 {
     echo $output;
 }
 
-    $input_file=($argc<2 ? "test.schema" : $argv[1]);
-    $variables=array(
-        "create"=>"1"
-    );
-    $arguments=array(
-        "Type"=>"mysql",
-        "User"=>"metapear",
-        "Password"=>"funky",
-        "Debug"=>"Output"
-    );
-    $manager=new metabase_manager_class;
-    $manager->debug="Output";
-    $success=$manager->UpdateDatabase($input_file,$input_file.".before",$arguments,$variables);
-    if($success)
-    {
-        echo $manager->DumpDatabase(array(
-            "Output"=>"Dump",
-            "EndOfLine"=>"\n"
-        ));
-    }
-    else
-        echo "Error: ".$manager->error."\n";
-    if(count($manager->warnings)>0)
-        echo "WARNING:\n",implode($manager->warnings,"!\n"),"\n";
-    if($manager->database)
-        echo MetabaseDebugOutput($manager->database);
+$input_file = ($argc<2 ? "test.schema" : $argv[1]);
+$variables = array(
+    "create" => "1"
+);
+$arguments = array(
+    "Type" => "mysql",
+    "User" => "metapear",
+    "Password" => "funky",
+    "Debug" => "Output"
+);
+$manager = new metabase_manager_class;
+$manager->debug = "Output";
+$success = $manager->UpdateDatabase($input_file, $input_file.".before", $arguments, $variables);
+if($success) {
+    echo $manager->DumpDatabase(array(
+        "Output" => "Dump",
+        "EndOfLine" => "\n"
+    ));
+} else {
+    echo "Error: ".$manager->error."\n";
+}
+if(count($manager->warnings) >0 ) {
+    echo "WARNING:\n",implode($manager->warnings,"!\n"),"\n";
+}
+if($manager->database) {
+    echo MetabaseDebugOutput($manager->database);
+}
 
 ?>
