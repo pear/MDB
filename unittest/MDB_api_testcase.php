0dcd5567ce2c84d927fce0c6997beb5798c3dfa4
Lukas Smith <lsmith@php.net>
2002-10-15 10:47:54 +0000

    added license moved to manual selection of the database instead of at connect time
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/trunk@99806 c90b9560-bf6c-de11-be94-00142212c4b1

--- unittest/MDB_api_testcase.php
+++ unittest/MDB_api_testcase.php
@@ -1,8 +1,47 @@
 <?php
-
-require_once '../manager.php';
-require_once '../date.php';
-require_once 'PHPUnit/PHPUnit.php';
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1998-2002 Manuel Lemos, Paul Cooper                    |
+// | All rights reserved.                                                 |
+// +----------------------------------------------------------------------+
+// | MDB is a merge of PEAR DB and Metabases that provides a unified DB   |
+// | API as well as database abstraction for PHP applications.            |
+// | This LICENSE is in the BSD license style.                            |
+// |                                                                      |
+// | Redistribution and use in source and binary forms, with or without   |
+// | modification, are permitted provided that the following conditions   |
+// | are met:                                                             |
+// |                                                                      |
+// | Redistributions of source code must retain the above copyright       |
+// | notice, this list of conditions and the following disclaimer.        |
+// |                                                                      |
+// | Redistributions in binary form must reproduce the above copyright    |
+// | notice, this list of conditions and the following disclaimer in the  |
+// | documentation and/or other materials provided with the distribution. |
+// |                                                                      |
+// | Neither the name of Manuel Lemos, Tomas V.V.Cox, Stig. S. Bakken,    |
+// | Lukas Smith nor the names of his contributors may be used to endorse |
+// | or promote products derived from this software without specific prior|
+// | written permission.                                                  |
+// |                                                                      |
+// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
+// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
+// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
+// | FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE      |
+// | REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,          |
+// | INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
+// | BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS|
+// |  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED  |
+// | AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT          |
+// | LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY|
+// | WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE          |
+// | POSSIBILITY OF SUCH DAMAGE.                                          |
+// +----------------------------------------------------------------------+
+// | Author: Paul Cooper <pgc@ucecom.com>                                 |
+// +----------------------------------------------------------------------+
+//
+// $Id$
 
 class MDB_Api_TestCase extends PHPUnit_TestCase {
     //contains the dsn of the database we are testing
@@ -19,9 +58,11 @@ class MDB_Api_TestCase extends PHPUnit_TestCase {
     }
 
     function setUp() {
-        global $dsn, $options;
+        global $dsn, $options, $database;
         $this->dsn = $dsn;
-        $this->db = MDB::connect($dsn, $options);
+        $this->database = $database;
+        $this->db =& MDB::connect($dsn, $options);
+        $this->db->setDatabase($this->database);
         $this->fields = array('user_name',
                         'user_password',
                         'subscribed',
@@ -94,7 +135,7 @@ class MDB_Api_TestCase extends PHPUnit_TestCase {
             $this->assertEquals("'Mr O\'Leary'", $text);
         }
     }
-    
+
     function testLoadExtension() {
         $this->assertTrue(FALSE, 'Test stub: please fill in');
     }
@@ -105,9 +146,7 @@ class MDB_Api_TestCase extends PHPUnit_TestCase {
         }
     }
 
-
     // test of the driver
-
     // helper function so that we don't have to write out a query a million times
     function standardQuery() {
         $query ="SELECT * FROM users";
@@ -118,11 +157,10 @@ class MDB_Api_TestCase extends PHPUnit_TestCase {
         return FALSE;
     }
 
-
     function testQuery() {
         if ($this->methodExists($this->db, 'query')) {
             $result = $this->standardQuery();
-            $this->assertTrue(is_resource($result), 'query: $result returned is not a resource');        
+            $this->assertTrue(is_resource($result), 'query: $result returned is not a resource');
         }
     }
 
