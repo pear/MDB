7e6711b9f20912ff2687467a10a091c5ae85dc6c
Paul Gregory Cooper <pgc@php.net>
2002-11-18 14:45:16 +0000

    Proper output for errors in both listeners
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/trunk@104752 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Console_TestListener.php
+++ tests/Console_TestListener.php
@@ -1,7 +1,8 @@
 <?php
 class Console_TestListener extends PHPUnit_TestListener {
     function addError(&$test, &$t) {
-        echo "MyListener::addError() called.\n";
+        $this->_errors += 1;
+        echo " Error $this->_errors in " . $test->getName() . " : $t\n";
     }
 
     function addFailure(&$test, &$t) {
@@ -9,20 +10,22 @@ class Console_TestListener extends PHPUnit_TestListener {
         if ($this->_fails == 1) {
             echo '\n';
         }
-        echo "Error $this->_fails : $t\n";
+        echo "Failure $this->_fails : $t\n";
     }
 
     function endTest(&$test) {
-        if ($this->_fails == 0) {
+        if ($this->_fails == 0 && $this->_errors == 0) {
             echo ' Test passed';
         } else {
             echo "There were $this->_fails failures for " . $test->getName() . "\n";
+            echo "There were $this->_errors errors for " . $test->getName() . "\n";
         }
         echo "\n";
     }
 
     function startTest(&$test) {
         $this->_fails = 0;
+        $this->_errors = 0;
         echo get_class($test) . " : Starting " . $test->getName() .  " ...";
     }
 }
--- tests/HTML_TestListener.php
+++ tests/HTML_TestListener.php
@@ -2,7 +2,8 @@
 
 class HTML_TestListener extends PHPUnit_TestListener {
     function addError(&$test, &$t) {
-        echo "MyListener::addError() called.\n";
+        $this->_errors += 1;
+        echo "<div class=\"error\"> Error $this->_errors in " . $test->getName() . " : $t</div>";
     }
 
     function addFailure(&$test, &$t) {
@@ -10,20 +11,22 @@ class HTML_TestListener extends PHPUnit_TestListener {
         if ($this->_fails == 1) {
             echo '<div class="failure">';
         }
-        echo "Error $this->_fails : $t<br>";
+        echo "Failure $this->_fails : $t<br>";
     }
 
     function endTest(&$test) {
-        if ($this->_fails == 0) {
+        if ($this->_fails == 0 && $this->_errors == 0) {
             echo ' Test passed';
         } else {
-            echo "There were $this->_fails failures for " . $test->getName() . "</div>";
+            echo "There were $this->_fails failures for " . $test->getName() . "</br>";
+            echo "There were $this->_errors errors for " . $test->getName() . "</div>";
         }
         echo "</div>";
     }
 
     function startTest(&$test) {
         $this->_fails = 0;
+        $this->_errors = 0;
         echo "\n<div class=\"test\">" . get_class($test) . " : Starting " . $test->getName() .  " ...";
     }
 }
