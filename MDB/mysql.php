f49b3a7846aff23a14202b397766358bc1f01575
Lukas Smith <lsmith@php.net>
2003-09-03 15:22:26 +0000

    cosmetic fix
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/trunk@139540 c90b9560-bf6c-de11-be94-00142212c4b1

--- MDB/mysql.php
+++ MDB/mysql.php
@@ -98,6 +98,8 @@ class MDB_mysql extends MDB_Common
         $this->supported['replace'] = 1;
         $this->supported['sub_selects'] = 0;
 
+        $this->options['default_table_type'] = null;
+
         $this->errorcode_map = array(
             1004 => MDB_ERROR_CANNOT_CREATE,
             1005 => MDB_ERROR_CANNOT_CREATE,
