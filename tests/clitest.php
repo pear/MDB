188a6c79cbf09446ad8f9a6784db03abeb3dd8b0
Lukas Smith <lsmith@php.net>
2003-09-20 13:42:20 +0000

    added pear error handler
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/trunk@140700 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/clitest.php
+++ tests/clitest.php
@@ -73,6 +73,14 @@ require_once 'testUtils.php';
 require_once 'MDB.php';
 require_once 'Console_TestListener.php';
 
+PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, 'handle_pear_error');
+function handle_pear_error ($error_obj)
+{
+    print '<pre><b>PEAR-Error</b><br />';
+    echo $error_obj->getMessage().': '.$error_obj->getUserinfo();
+    print '</pre>';
+}
+
 MDB::loadFile('Date');
 
 foreach ($testcases as $testcase) {
