6b5a7c32df15a7af6b196c57ba6d0ffdefe8eaaf
Lukas Smith <lsmith@php.net>
2004-02-25 17:29:36 +0000

    - minor fixes (we still pass the wrong order to many assertEquals calls) - added currId() testing to the sequence test
    git-svn-id: http://svn.php.net/repository/pear/packages/MDB/branches/MDB_1@152371 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/MDB_usage_testcase.php
+++ tests/MDB_usage_testcase.php
@@ -148,7 +148,7 @@ class MDB_Usage_TestCase extends PHPUnit_TestCase {
             }
             $value = $row[$i];
             $field = $this->fields[$i];
-            $this->assertEquals($value, $data[$field], "the value retrieved for field \"$field\" ($value) using type $type doesn't match what was stored ($data[$field]).", $delta);
+            $this->assertEquals($data[$field], $value, "the value retrieved for field \"$field\" ($value) using type $type doesn't match what was stored ($data[$field]).", $delta);
         }
     }
 
@@ -410,7 +410,7 @@ class MDB_Usage_TestCase extends PHPUnit_TestCase {
         for($string = 0; $string < count($test_strings); $string++) {
             $this->clearTables();
 
-            $value = $this->db->getTextValue($test_strings[$string]);
+            $value = $this->db->getValue('text', $test_strings[$string]);
 
             $result = $this->db->query("INSERT INTO users (user_name,user_password,user_id) VALUES ($value,$value,0)");
 
@@ -429,7 +429,7 @@ class MDB_Usage_TestCase extends PHPUnit_TestCase {
             $value = $this->db->fetch($result, 0, 'user_name');
             $this->db->freeResult($result);
 
-            $this->assertEquals(rtrim($value), $test_strings[$string], "the value retrieved for field \"user_name\" (\"$value\") doesn't match what was stored (".$test_strings[$string].')');
+            $this->assertEquals($test_strings[$string], rtrim($value), "the value retrieved for field \"user_name\" (\"$value\") doesn't match what was stored (".$test_strings[$string].')');
 
         }
     }
@@ -502,7 +502,7 @@ class MDB_Usage_TestCase extends PHPUnit_TestCase {
 
             $result_rows = $this->db->numRows($result);
 
-            $this->assertTrue(($result_rows <= $rows), 'expected a result of no more than $rows but the returned number of rows is $result_rows');
+            $this->assertTrue(($result_rows <= $rows), "expected a result of no more than $rows but the returned number of rows is $result_rows");
 
             for ($row = 0; $row < $result_rows; $row++) {
                 $this->assertTrue(!$this->db->endOfResult($result), 'The query result seem to have reached the end of result at row $row that is before $result_rows as expected');
@@ -558,6 +558,30 @@ class MDB_Usage_TestCase extends PHPUnit_TestCase {
 
         $result = $this->db->dropSequence($sequence_name);
 
+        if (MDB::isError($result)) {
+            $this->assertTrue(false, "Error dropping sequence $sequence_name : ".$result->getMessage());
+        }
+
+        // Test currId()
+        $sequence_name = 'test_currid';
+
+        $result = $this->db->createSequence($sequence_name);
+
+        $next = $this->db->nextId($sequence_name);
+        $curr = $this->db->currId($sequence_name);
+
+        if (MDB::isError($curr)) {
+            $this->assertTrue(false, "Error getting the current value of sequence $sequence_name : ".$curr->getMessage());
+        } else {
+            if ($next != $curr) {
+                if ($next+1 == $curr) {
+                    $this->assertTrue(false, "Warning: currId() is using nextId() instead of a native implementation");
+                } else {
+                    $this->assertEquals($next, $curr, "return value if currId() does not match the previous call to nextId()");
+                }
+            }
+        }
+        $result = $this->db->dropSequence($sequence_name);
         if (MDB::isError($result)) {
             $this->assertTrue(false, "Error dropping sequence $sequence_name : ".$result->getMessage());
         }
@@ -1079,18 +1103,13 @@ class MDB_Usage_TestCase extends PHPUnit_TestCase {
         if (!$this->methodExists('tableInfo')) {
             return;
         }
-        if (MDB::isError($connect = $this->db->connect())) {
-            $this->assertTrue(false, 'Cannot connect to the db'.$connect->getMessage());
-        }
 
         $table_info = $this->db->tableInfo('users');
-        //mydebug($table_info);
         if (MDB::isError($table_info)) {
             $this->assertTrue(false, 'Error in tableInfo(): '.$table_info->getMessage());
         } else {
 
-
-            $this->assertEquals(count($table_info), count($this->fields), 'The number of fields retrieved ('.count($table_info).') is different from the expected one ('.count($this->fields).')');
+            $this->assertEquals(count($this->fields), count($table_info), 'The number of fields retrieved ('.count($table_info).') is different from the expected one ('.count($this->fields).')');
             foreach ($table_info as $field_info) {
                 $this->assertEquals($field_info['table'], 'users', "the table name is not correct (expected: 'users'; actual: $field_info[table])");
                 if (!in_array(strtolower($field_info['name']), $this->fields)) {
